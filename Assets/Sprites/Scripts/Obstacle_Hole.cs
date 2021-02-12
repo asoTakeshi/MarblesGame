6a965602ea95bb6a83117738c658d57b01e577b9
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-13 00:15:38 +0900

    手順14

---
+++ Assets/Sprites/Scripts/Obstacle_Hole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Hole : ObstacleBase
+{
+    // オーバーライドして利用する
+    protected override void BeforeTriggerEffect(CharaBall charaBall)
+    {
+        // 手球を１つ減らす
+        //charaBall.UpdateHp(-power);
+
+        // スタート位置へ戻す
+        //StartCoroutine(battleManager.RestartCharaPosition(2.0f));
+    }
+}
+
+
